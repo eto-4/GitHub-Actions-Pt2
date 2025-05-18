9ad5496d8be88d918b7210cdd8c8c7888f535387
eto <aleix.murt22@lacetania.cat>
2025-05-18 16:41:32 +0200

    Update resultatsAPI.py
    Correció al nom final del fitxer

--- resultatsAPI.py
+++ resultatsAPI.py
@@ -114,7 +114,8 @@ datos_json = {
     }
 }
 
-date = datetime.strptime("20250516" ,"%Y%m%d").date()
+date = datetime.strptime("20250516", "%Y%m%d")
+date_str = date.strftime("%Y%m%d")
 
-with open(f"dades_json/temperatura_{date}.json", "w", encoding="utf-8") as f:
+with open(f"dades_json/temperatura_{date_str}.json", "w", encoding="utf-8") as f:
     json.dump(datos_json, f, indent=4, ensure_ascii=False)
